dcee0125ca14dbeb1fedf5b2a19a917e0e81a091
Osada Paranaliyanage <osadalakmal@gmail.com>
2013-10-13 22:26:42 +0100

    corrected tokenize implementation

--- parser.t.cpp
+++ parser.t.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 
 TEST(Parser,Tokenize) {
-    Parser parser("Hello World");
+    Parser parser("(define operateTwice(lambda(op1 op2)(lambda(x y) (op2 (op1 x y) y))))");
     std::string result;
     boost::sregex_token_iterator it = parser.Tokenize(result);
     boost::sregex_token_iterator j;
+
+    while(it != j)
+    {
+        std::cout << *it++ << std::endl;
+    }
 }
